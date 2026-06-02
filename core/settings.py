82bcd9087e24c5621e571ffb694a2e00f672a389
support-ai-solutions <custom.new.solutions@gmail.com>
2026-06-02 12:59:36 +0330

    debug

--- core/settings.py
+++ core/settings.py
@@ -32,8 +32,9 @@ load_dotenv()
 ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    # "http://localhost:3000",
+    # "http://127.0.0.1:3000",
+    "*"
 ]
 
 # Application definition
